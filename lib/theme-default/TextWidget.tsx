66b5f2c2277e48c948b31a0957f49816852c9f65
Lin Zy <30574921+euyZL@users.noreply.github.com>
2021-06-19 16:41:04 +0800

    Update TextWidget.tsx

--- lib/theme-default/TextWidget.tsx
+++ lib/theme-default/TextWidget.tsx
@@ -6,44 +6,42 @@
  * @Description: In User Settings Edit
  * @FilePath: \vue3-json-schema-form\lib\theme-default\TextWidget.tsx
  */
-import { defineComponent, computed, markRaw } from 'vue'
+import { defineComponent, computed } from 'vue'
 import { CommonWidgetDefine, CommonWidgetPropsDefine } from '../types'
 import { withFormItem } from './FormItem'
 
-const TextWidget: CommonWidgetDefine = markRaw(
-  withFormItem(
-    defineComponent({
-      name: 'TextWidget',
-      props: CommonWidgetPropsDefine,
-      setup(props) {
-        const handleChange = (e: any) => {
-          // console.log(e)
-          const value = e.target.value
-          e.target.value = props.value //controlled-input实现
-          props.onChange(value)
+const TextWidget: CommonWidgetDefine = withFormItem(
+  defineComponent({
+    name: 'TextWidget',
+    props: CommonWidgetPropsDefine,
+    setup(props) {
+      const handleChange = (e: any) => {
+        // console.log(e)
+        const value = e.target.value
+        e.target.value = props.value //controlled-input实现
+        props.onChange(value)
+      }
+
+      const styleRef = computed(() => {
+        return {
+          color: (props.options && props.options.color) || 'black',
         }
+      })
 
-        const styleRef = computed(() => {
-          return {
-            color: (props.options && props.options.color) || 'black',
-          }
-        })
+      return () => {
+        const { value } = props
 
-        return () => {
-          const { value } = props
-
-          return (
-            <input
-              type="text"
-              value={value as any}
-              onInput={handleChange}
-              style={styleRef.value}
-            />
-          )
-        }
-      },
-    }),
-  ),
+        return (
+          <input
+            type="text"
+            value={value as any}
+            onInput={handleChange}
+            style={styleRef.value}
+          />
+        )
+      }
+    },
+  }),
 )
 
 export default TextWidget
